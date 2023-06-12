590f39ee105f303bef650c3f972399d78bb886e7
abacusgiesinger <94472542+abacusgiesinger@users.noreply.github.com>
2023-06-12 14:31:56 +0000

    Change ios version from 16 to 15

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ import PackageDescription
 let package = Package(
     name: "swift-openapi-urlsession",
     platforms: [
-        .macOS(.v13), .iOS(.v16), .tvOS(.v16), .watchOS(.v9),
+        .macOS(.v13), .iOS(.v15), .tvOS(.v16), .watchOS(.v9),
     ],
     products: [
         .library(
